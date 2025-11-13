648def19b983901fcf49d8dddbbb8ab2d2b00fa0
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2025-11-13 21:21:20 +0100

    Add missing className to Image component in Hero section for responsive design

--- frontend/src/app/containers/Home/sections/Hero.tsx
+++ frontend/src/app/containers/Home/sections/Hero.tsx
@@ -20,6 +20,7 @@ export const Hero = ({
           alt="XCG Coffee Logo"
           width={412 / 2}
           height={64 / 2}
+          className="max-w-[10rem] md:max-w-0"
         />
         <Button onClick={handleClickCreateCoffe}>Create</Button>
       </div>
